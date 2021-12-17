2a27d70c181b29e121f2627ca033efc8334ec34c
Thamyres Lais <thamyreslais28@gmail.com>
2021-12-17 10:46:50 -0300

    mais um erro

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from 'react/cjs/react.development'
+import { useState } from 'react/cjs/react.development'
 import cities from '../../data/database.json'
 import './cards.styles.css'
 
