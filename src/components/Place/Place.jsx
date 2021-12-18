525d3d62e22b2158f89ad4bb2fcc51c1810a8d9f
Thamyres Lais <thamyreslais28@gmail.com>
2021-12-17 22:19:53 -0300

    erro resolvido link

--- src/components/Place/Place.jsx
+++ src/components/Place/Place.jsx
@@ -11,24 +11,24 @@ function Place(){
         <section>
             <div className="art-container">
                 {cities.map((places) =>
-                    <ul className='artes-container'>
-                        <li className='artes' key={places.id}>
+                    <ul key={places.id} className='artes-container'>
+                        <li className='artes'>
                             <h1>{places.name}</h1>
                             <img className="artes-foto" src={places.imageArt1} alt={places.nameArt1 }/>
                             <p>{places.nameArt1}</p>
                         
                         </li>
-                        <li className='artes' key={places.id}>
+                        <li className='artes'>
                             <img className="artes-foto" src={places.imageArt2} alt={places.nameArt2}/>
                             <p>{places.nameArt2}</p>
 
                         </li>
-                        <li className='artes' key={places.id}>
+                        <li className='artes'>
                             <img className="artes-foto" src={places.imageArt3} alt={places.nameArt3}/>
                             <p>{places.nameArt3}</p>
 
                         </li>
-                        <li className='artes' key={places.id}>
+                        <li className='artes'>
                             <img className="artes-foto" src={places.imageArt4} alt={places.nameArt4}/>
                             <p>{places.nameArt4}</p>
                         </li>
